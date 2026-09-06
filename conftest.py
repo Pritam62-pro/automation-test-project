9a6ceb9a7285c7b7733656c92edf030f6b5c0461
pritam <pritambiswas1072@gmail.com>
2026-09-06 12:11:29 +0530

    handdle add

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
 
 def pytest_addoption(parser):
@@ -13,9 +15,20 @@ def driver(request):
     browser_name = request.config.getoption("--browser")
     
     if browser_name == "chrome":
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        chrome_options = ChromeOptions()
+        # GitHub server e display nei, tai headless mode lagbe
+        chrome_options.add_argument('--headless') 
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        
     elif browser_name == "firefox":
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument('--headless') # Firefox er jonno headless
+        
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options)
+        
     else:
         raise ValueError(f"Browser {browser_name} not supported")
         
